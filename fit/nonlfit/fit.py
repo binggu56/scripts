17d5f6eab31cd8c5aaed0b142ba61d4d2b83ddce
Bing Gu <bingg@email.sc.edu>
2014-07-15 15:08:43 -0400

    small update

--- fit/nonlfit/fit.py
+++ fit/nonlfit/fit.py
@@ -1,11 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python 
+
+# non-linear fitting 
+
 import numpy as np
 from scipy.optimize import curve_fit
 import pylab as pl
 
+
 def func(x, a, b, c):
+    """fitting function with unknown variables
+    """
     return a*np.exp(-b*x) + c
 
+
+# data points (x,y)
 x = np.linspace(0,4,50)
 y = func(x, 2.5, 1.3, 0.5)
 
@@ -13,8 +21,7 @@ yn = y + 0.2*np.random.normal(size=len(x))
 
 p0 = [0.5,0.5,0.6]
 
-# p0    : initial guess
-# sigma : data points weights 
+# p0: initial guess; sigma : data points weights 
 popt, pcov = curve_fit(func, x, yn, p0,sigma=None)
 
 #print(curve_fit.__doc__)
@@ -25,5 +32,6 @@ pl.subplot(111)
 pl.plot(x,yn,'-',label='source')
 pl.plot(x,yfit,'r--',label='fit')
 pl.legend()
-pl.show()
 pl.savefig('fit.pdf')
+pl.show()
+
